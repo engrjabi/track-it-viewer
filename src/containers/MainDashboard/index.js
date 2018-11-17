679607531365668e6ea9acbdbafcbcedc62e3508
Joshua Ignacio <joshua.ignacio@dynameyes.com>
2018-11-17 14:47:11 +0800

    initial working prototype of folder/files image viewer

--- src/containers/MainDashboard/index.js
+++ src/containers/MainDashboard/index.js
@@ -6,30 +6,144 @@ import {
 	makeGetVeryDeepStateAndGreetings,
 	makeGetVeryDeepState,
 } from '../../ducks/selectors/mainDashBoard';
+import Dropzone from 'react-dropzone'
+import { fromEvent } from 'file-selector';
+import { withStyles } from '@material-ui/core/styles';
+import CardList from '../../components/CardList';
+import { sortAlphabetically } from '../../utils/Formatters';
+import Select from 'react-select';
 import './style.css';
 
+const styles = theme => ({
+	root: {
+		display: 'flex',
+		flexWrap: 'wrap',
+		justifyContent: 'space-around',
+		overflow: 'hidden',
+		backgroundColor: theme.palette.background.paper,
+	},
+	gridList: {
+		width: 500,
+		height: 450,
+	},
+	subheader: {
+		width: '100%',
+	},
+	mainDateSelector: {
+		zIndex: 11,
+		display: 'inline-block',
+		margin: 20,
+		width: '50%',
+	},
+	dropZone: {
+		display: 'inline-block',
+		margin: 20,
+	},
+	dropZoneContainer: {
+		padding: 10,
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		height: 180,
+		textAlign: 'center',
+	}
+});
+
 class MainDashBoard extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			files: [],
+			selectedGroup: {},
+			groupDateOptions: [],
+			loadingFiles: false,
+		}
+	}
+
 	componentDidMount() {
 		const { toggleGreetings, toggleNestedState } = this.props;
 		toggleGreetings('hello', 'world', 'hi');
 		toggleNestedState('foo', 'bar', 'bar');
 	}
 
+	handleChangeOfDateFilter = (selectedGroup) => {
+		this.setState({ selectedGroup });
+	};
+
+	onDrop = (files) => {
+		let groupDateOptions = [];
+		const formattedFiles = files.sort(sortAlphabetically('path'))
+			.reduce((accumulator, file) => {
+				const splitPaths = file.path.split('/');
+				const date = splitPaths[1];
+
+				if (accumulator[date]) {
+					accumulator[date].push({
+						img: URL.createObjectURL(file),
+						title: file.name,
+					})
+				} else {
+					groupDateOptions.push({
+						value: date,
+						label: date,
+					});
+					accumulator[date] = [{
+						img: URL.createObjectURL(file),
+						title: file.name,
+					}];
+				}
+
+				return accumulator;
+			}, {});
+		console.log('end', formattedFiles, groupDateOptions);
+		this.setState({
+			files: formattedFiles,
+			groupDateOptions,
+			loadingFiles: false,
+		})
+	};
+
+	handleData = (e) => {
+		if (e.type === 'drop') this.setState({ loadingFiles: true });
+		return fromEvent(e);
+	};
+
 	render() {
-		const { veryDeepStateAndGreetings, greetings, veryDeepState } = this.props;
+		const { classes } = this.props;
+		const { files, selectedGroup, loadingFiles, groupDateOptions } = this.state;
+
 		return (
-			<div className="App">
-				<header className="App-header">
-					<h1 className="App-title">Welcome to React</h1>
-				</header>
-				<div className="App-intro">
-					Redux State Connected:
-					<p><strong>greetings:</strong> {greetings}</p>
-					<p><strong>veryDeepState:</strong> {veryDeepState}</p>
-					<p><strong>veryDeepStateAndGreetings:</strong> {veryDeepStateAndGreetings}</p>
+			<section>
+				<div className={classes.dropZone}>
+					<Dropzone
+						disableClick
+						disabled={loadingFiles}
+						getDataTransferItems={this.handleData}
+						onDrop={this.onDrop}
+					>
+						<div className={classes.dropZoneContainer}>
+							<p>Drop a folder with files here.</p>
+						</div>
+
+					</Dropzone>
 				</div>
-			</div>
-		);
+
+				{groupDateOptions.length !== 0 &&
+				<Select
+					style={{
+						zIndex: 11,
+					}}
+					className={classes.mainDateSelector}
+					value={selectedGroup}
+					onChange={this.handleChangeOfDateFilter}
+					options={groupDateOptions}
+				/>}
+
+				<CardList
+					cardList={files[selectedGroup.value] || []}
+				/>
+			</section>
+		)
 	}
 }
 
@@ -44,4 +158,4 @@ const mapDispatchToProps = {
 	toggleNestedState: mainDashBoardActions.toggleNestedState,
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(MainDashBoard);
+export default withStyles(styles)(connect(mapStateToProps, mapDispatchToProps)(MainDashBoard));
